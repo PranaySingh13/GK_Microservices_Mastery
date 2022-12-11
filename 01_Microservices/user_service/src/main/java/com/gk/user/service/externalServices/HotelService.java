b754c613cc6e4f75a3b9468c4fc02f9224a940eb
PranaySingh13 <pranay.singh.1393@gmail.com>
2022-12-11 19:31:40 +0530

    Spring Cloud OpenFeign Client

---
+++ 01_Microservices/user_service/src/main/java/com/gk/user/service/externalServices/HotelService.java
@@ -0,0 +1,14 @@
+package com.gk.user.service.externalServices;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.gk.user.service.entity.Hotel;
+
+@FeignClient(name = "HOTEL-SERVICE")
+public interface HotelService {
+
+	@GetMapping("/api/hotels/hotel/{hotelId}")
+	Hotel getHotel(@PathVariable String hotelId);
+}
---
+++ 01_Microservices/user_service/src/main/java/com/gk/user/service/externalServices/RatingService.java
@@ -0,0 +1,15 @@
+package com.gk.user.service.externalServices;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.gk.user.service.entity.Rating;
+
+@FeignClient(name = "RATING-SERVICE")
+public interface RatingService {
+
+	@PostMapping("/api/ratings/save")
+	ResponseEntity<Rating> createRating(Rating rating);
+
+}
