93b92ef10e4f9d38801d40d81da8bcc15a4ba1e4
PranaySingh13 <pranay.singh.1393@gmail.com>
2023-02-10 02:01:27 +0530

    @EnableEurekaClient is deprecated hence removed from all client services only its dependency spring-cloud-starter-netflix-eureka-client is needed in pom.xml
    Signed-off-by: PranaySingh13 <pranay.singh.1393@gmail.com>

--- 01_Microservices/user_service/src/main/java/com/gk/user/service/UserServiceApplication.java
+++ 01_Microservices/user_service/src/main/java/com/gk/user/service/UserServiceApplication.java
@@ -2,10 +2,19 @@ package com.gk.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+//@EnableEurekaClient
+/*
+ * On Spring Boot 3.0.0 and Java 17, @EnableEurekaClient is deprecated no need
+ * to annotate at spring boot main application it is fine if we add the
+ * spring-cloud-starter-netflix-eureka-client dependency in pom and if we have
+ * the application name in yml or properties file it will be registered to
+ * Eureka Server.
+ */
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
